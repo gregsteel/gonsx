b91300c4c6ddf9b4d03463d2d1ce594eb2aafc5d
Greg Steel <greg.steel@gmail.com>
2018-11-19 18:05:33 +1100

    support edge gateway features

--- api/edgegateway/edgegateway_types.go
+++ api/edgegateway/edgegateway_types.go
@@ -11,29 +11,30 @@ type EdgeGateways struct {
 
 // EdgeGateway object within EdgeGateways list.
 type EdgeGateway struct {
-	XMLName        xml.Name              `xml:"edge"`
-	Id             string                `xml:"id,omitempty"`
-	DatacenterMoid string                `xml:"datacenterMoid"`
-	Name           string                `xml:"name"`
-	Type           string                `xml:"type,omitempty"`
-	Tenant         string                `xml:"tenant,omitempty"`
-	Fqdn           string                `xml:"fqdn,omitempty"`
-	VseLogLevel    string                `xml:"vseLogLevel,omitempty"`
-	EnableAesni    bool                  `xml:"enableAesni,omitempty"`
-	EnableFips     bool                  `xml:"enableFips,omitempty"`
-	Appliances     EdgeGatewayAppliances `xml:"appliances"`
-	Vnics          EdgeVnics             `xml:"vnics"`
+	XMLName        xml.Name   `xml:"edge"`
+	Id             string     `xml:"id,omitempty"`
+	DatacenterMoid string     `xml:"datacenterMoid"`
+	Name           string     `xml:"name"`
+	Type           string     `xml:"type,omitempty"`
+	Tenant         string     `xml:"tenant,omitempty"`
+	Fqdn           string     `xml:"fqdn,omitempty"`
+	VseLogLevel    string     `xml:"vseLogLevel,omitempty"`
+	EnableAesni    bool       `xml:"enableAesni,omitempty"`
+	EnableFips     bool       `xml:"enableFips,omitempty"`
+	Appliances     Appliances `xml:"appliances"`
+	Vnics          EdgeVnics  `xml:"vnics"`
+	Features       Features   `xml:"features"`
 }
 
 // Appliances within EdgeGateway.
-type EdgeGatewayAppliances struct {
-	ApplianceSize  string                 `xml:"applianceSize"`
-	EnableCoreDump bool                   `xml:"enableCoreDump,omitempty"`
-	ApplianceList  []EdgeGatewayAppliance `xml:"appliance"`
+type Appliances struct {
+	ApplianceSize  string      `xml:"applianceSize"`
+	EnableCoreDump bool        `xml:"enableCoreDump,omitempty"`
+	ApplianceList  []Appliance `xml:"appliance"`
 }
 
 // Appliance object within Appliance list.
-type EdgeGatewayAppliance struct {
+type Appliance struct {
 	XMLName        xml.Name `xml:"appliance"`
 	ResourcePoolId string   `xml:"resourcePoolId"`
 	DatastoreId    string   `xml:"datastoreId"`
@@ -55,6 +56,151 @@ type EdgeVnic struct {
 	PortgroupId   string        `xml:"portgroupId"`
 	IsConnected   bool          `xml:"isConnected"`
 	AddressGroups AddressGroups `xml:"addressGroups"`
+}
+
+// Features within EdgeGateway.
+type Features struct {
+	//lots of other features to be added
+	Firewall Firewall `xml:"firewall"`
+	Routing  Routing  `xml:"routing"`
+	Dhcp     Dhcp     `xml:"dhcp"`
+}
+
+// Firewall object within Edge features list.
+type Firewall struct {
+	DefaultPolicy FirewallDefaultPolicy `xml:"defaultPolicy"`
+	GlobalConfig  FirewallGlobalConfig  `xml:"globalConfig"`
+}
+
+// FirewallDefaultPolicy ...
+type FirewallDefaultPolicy struct {
+	Action         string `xml:"action"`
+	LoggingEnabled bool   `xml:"loggingEnabled"`
+}
+
+// FirewallGlobalConfig ...
+type FirewallGlobalConfig struct {
+	//lots of other config
+	TcpTimeoutEstablished int `xml:"tcpTimeoutEstablished"`
+}
+
+// Routing object within Edge features list.
+type Routing struct {
+	GlobalConfig  RoutingGlobalConfig `xml:"routingGlobalConfig"`
+	StaticRouting StaticRouting       `xml:"staticRouting"`
+	OSPFRouting   OSPFRouting         `xml:"ospf"`
+}
+
+//RoutingGlobalConfig ...
+type RoutingGlobalConfig struct {
+	RouterId string `xml:"routerId"`
+	ECMP     bool   `xml:"ecmp"`
+	//Logging 	RoutingGlobalConfigLogging 	`xml:"logging"`
+}
+
+//StaticRouting ...
+type StaticRouting struct {
+	DefaultRoute StaticRoutingDefault `xml:"defaultRoute"`
+	//StaticRoutes 	StaticRoutes 			`xml:"staticRoutes"`
+}
+
+//StaticRoutingDefault ...
+type StaticRoutingDefault struct {
+	Description    string `xml:"description"`
+	Vnic           int    `xml:"vnic"`
+	GatewayAddress string `xml:"gatewayAddress"`
+	Mtu            int    `xml:"mtu"`
+}
+
+//OSPFRouting ...
+type OSPFRouting struct {
+	Enabled          bool               `xml:"enabled"`
+	OSPFAreas        OSPFAreas          `xml:"ospfAreas"`
+	OSPFInterfaces   OSPFInterfaces     `xml:"ospfInterfaces"`
+	Redistribution   OSPFRedistribution `xml:"redistribution"`
+	GracefulRestart  bool               `xml:"gracefulRestart"`
+	DefaultOriginate bool               `xml:"defaultOriginate"`
+}
+
+//OSPFAreas ...
+type OSPFAreas struct {
+	OSPFAreaList []OSPFArea `xml:"ospfArea"`
+}
+
+//OSPFArea ...
+type OSPFArea struct {
+	AreaId int    `xml:"areaId"`
+	Type   string `xml:"type"`
+}
+
+//OSPFInterfaces ...
+type OSPFInterfaces struct {
+	OSPFInterfaceList []OSPFInterface `xml:"ospfInterface"`
+}
+
+//OSPFInterface ...
+type OSPFInterface struct {
+	Vnic   int `xml:"vnic"`
+	AreaId int `xml:"areaId"`
+	//more attributes ...
+}
+
+//OSPFRedistribution ...
+type OSPFRedistribution struct {
+	Enabled bool       `xml:"enabled"`
+	Rules   []OSPFRule `xml:"rules"`
+	//more attributes ...
+}
+
+//OSPFRule ...
+type OSPFRule struct {
+	//??
+}
+
+//Dhcp
+type Dhcp struct {
+	StaticBindings DhcpStaticBindings `xml:"staticBindings"`
+	IpPools        DhcpIpPools        `xml:"ipPools"`
+}
+
+//DhcpStaticBindings
+type DhcpStaticBindings struct {
+	StaticBindingList []DhcpStaticBinding `xml:"staticBinding"`
+}
+
+//DhcpStaticBinding
+type DhcpStaticBinding struct {
+	MacAddress          string `xml:"macAddress"`
+	VmId                string `xml:"vmId"`
+	VnicId              int    `xml:"vnicId"`
+	Hostname            string `xml:"hostname"`
+	IpAddress           string `xml:"ipAddress"`
+	SubnetMask          string `xml:"subnetMask"`
+	DefaultGateway      string `xml:"defaultGateway"`
+	DomainName          string `xml:"domainName"`
+	PrimaryNameServer   string `xml:"primaryNameServer"`
+	SecondaryNameServer string `xml:"secondaryNameServer"`
+	LeaseTime           string `xml:"leaseTime"`
+	AutoConfigureDNS    bool   `xml:"autoConfigureDNS"`
+}
+
+//IpPools
+type DhcpIpPools struct {
+	IpPoolList []DhcpIpPool `xml:"ipPool"`
+}
+
+//DhcpIpPool
+type DhcpIpPool struct {
+	IpRange             string `xml:"ipRange"`
+	DefaultGateway      string `xml:"defaultGateway"`
+	SubnetMask          string `xml:"subnetMask"`
+	DomainName          string `xml:"domainName"`
+	PrimaryNameServer   string `xml:"primaryNameServer"`
+	SecondaryNameServer string `xml:"secondaryNameServer"`
+	LeaseTime           string `xml:"leaseTime"`
+	AutoConfigureDNS    bool   `xml:"autoConfigureDNS"`
+	NextServer          string `xml:"nextServer,omitempty"`
+	//dhcpOptions ...
 }
 
 // AddressGroups within EdgeVnic.
--- api/edgegateway/edgegateway_types_functions_test.go
+++ api/edgegateway/edgegateway_types_functions_test.go
@@ -16,13 +16,83 @@ func constructEdgeGateway(edgeId string, name string) (edgeGateway *EdgeGateway)
 		Name:           name,
 		DatacenterMoid: "dc-1",
 		Tenant:         "default",
-		Appliances: EdgeGatewayAppliances{
+		Appliances: Appliances{
 			ApplianceSize:  "compact",
 			EnableCoreDump: true,
-			ApplianceList: []EdgeGatewayAppliance{
-				EdgeGatewayAppliance{
+			ApplianceList: []Appliance{
+				Appliance{
 					ResourcePoolId: "pool-1",
 					DatastoreId:    "datastore-1",
+				},
+			},
+		},
+		Features: Features{
+			Firewall: Firewall{
+				DefaultPolicy: FirewallDefaultPolicy{
+					Action:         "accept",
+					LoggingEnabled: false,
+				},
+				GlobalConfig: FirewallGlobalConfig{
+					TcpTimeoutEstablished: 300000,
+				},
+			},
+			Routing: Routing{
+				GlobalConfig: RoutingGlobalConfig{
+					RouterId: "1.1.1.1",
+				},
+				StaticRouting: StaticRouting{
+					DefaultRoute: StaticRoutingDefault{
+						Description:    "defaultRoute",
+						Vnic:           0,
+						GatewayAddress: "1.1.1.1",
+						Mtu:            150,
+					},
+				},
+				OSPFRouting: OSPFRouting{
+					Enabled: true,
+					OSPFAreas: OSPFAreas{
+						[]OSPFArea{
+							OSPFArea{
+								AreaId: 0,
+								Type:   "normal",
+							},
+						},
+					},
+					OSPFInterfaces: OSPFInterfaces{
+						[]OSPFInterface{
+							OSPFInterface{
+								Vnic:   0,
+								AreaId: 0,
+							},
+						},
+					},
+					Redistribution: OSPFRedistribution{
+						Enabled: false,
+						Rules:   []OSPFRule{},
+					},
+					GracefulRestart:  true,
+					DefaultOriginate: false,
+				},
+			},
+			Dhcp: Dhcp{
+				StaticBindings: StaticBindings{
+					StaticBindingList: []StaticBinding{
+						StaticBinding{
+							MacAddress: "abcd",
+							VmId:       "vm123",
+							IpAddress:  "2.2.2.2",
+						},
+					},
+				},
+				IpPools: IpPools{
+					IpPoolList: []IpPool{
+						IpPool{
+							IpRange:          "10.1.1.1-10.2.2.2",
+							DefaultGateway:   "1.1.1.1",
+							SubnetMask:       "255.255.255.0",
+							AutoConfigureDNS: true,
+						},
+					},
 				},
 			},
 		},
@@ -33,7 +103,7 @@ func constructEdgeGateway(edgeId string, name string) (edgeGateway *EdgeGateway)
 func TestMarshalToXML(t *testing.T) {
 	edgeGateway := constructEdgeGateway("edge-1", "Edge Gateway Test")
 	convertedXML := edgeGateway.MarshalToXML()
-	expectedXML := "<edge><id>edge-1</id><datacenterMoid>dc-1</datacenterMoid><name>Edge Gateway Test</name><tenant>default</tenant><appliances><applianceSize>compact</applianceSize><enableCoreDump>true</enableCoreDump><appliance><resourcePoolId>pool-1</resourcePoolId><datastoreId>datastore-1</datastoreId><vmFolderId></vmFolderId></appliance></appliances><vnics></vnics></edge>"
+	expectedXML := "<edge><id>edge-1</id><datacenterMoid>dc-1</datacenterMoid><name>Edge Gateway Test</name><tenant>default</tenant><appliances><applianceSize>compact</applianceSize><enableCoreDump>true</enableCoreDump><appliance><resourcePoolId>pool-1</resourcePoolId><datastoreId>datastore-1</datastoreId><vmFolderId></vmFolderId></appliance></appliances><vnics></vnics><features><firewall><defaultPolicy><action>accept</action><loggingEnabled>false</loggingEnabled></defaultPolicy><globalConfig><tcpTimeoutEstablished>300000</tcpTimeoutEstablished></globalConfig></firewall><routing><routingGlobalConfig><routerId>1.1.1.1</routerId><ecmp>false</ecmp></routingGlobalConfig><staticRouting><defaultRoute><description>defaultRoute</description><vnic>0</vnic><gatewayAddress>1.1.1.1</gatewayAddress><mtu>150</mtu></defaultRoute></staticRouting><ospf><enabled>true</enabled><ospfAreas><ospfArea><areaId>0</areaId><type>normal</type></ospfArea></ospfAreas><ospfInterfaces><ospfInterface><vnic>0</vnic><areaId>0</areaId></ospfInterface></ospfInterfaces><redistribution><enabled>false</enabled></redistribution><gracefulRestart>true</gracefulRestart><defaultOriginate>false</defaultOriginate></ospf></routing><dhcp><staticBindings><staticBinding><macAddress>abcd</macAddress><vmId>vm123</vmId><vnicId>0</vnicId><hostname></hostname><ipAddress>2.2.2.2</ipAddress><subnetMask></subnetMask><defaultGateway></defaultGateway><domainName></domainName><primaryNameServer></primaryNameServer><secondaryNameServer></secondaryNameServer><leaseTime></leaseTime><autoConfigureDNS>false</autoConfigureDNS></staticBinding></staticBindings><ipPools><ipPool><ipRange>10.1.1.1-10.2.2.2</ipRange><defaultGateway>1.1.1.1</defaultGateway><subnetMask>255.255.255.0</subnetMask><domainName></domainName><primaryNameServer></primaryNameServer><secondaryNameServer></secondaryNameServer><leaseTime>0</leaseTime><autoConfigureDNS>true</autoConfigureDNS></ipPool></ipPools></dhcp></features></edge>"
 	assert.Equal(t, expectedXML, convertedXML)
 }
 
